944e02a09714071abd89159f77df70cc7e48a911
ivanevacic <ivanevacicweb@gmail.com>
2018-01-26 22:06:42 +0100

    Fixed 'font-awesome' gulp error

--- gulpfile.js
+++ gulpfile.js
@@ -2,52 +2,41 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-//Compile Sass & inject into browser
-gulp.task('sass', ()  => {
-  //Compile all of our Sass
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss',
-                   'src/scss/*.scss'])
-                      .pipe(sass())
-                      .pipe(gulp.dest('src/css'))
-                      .pipe(browserSync.stream());
+// Compile Sass & Inject Into Browser
+gulp.task('sass', () => {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
-//Move JavaScript files to src/js
+// Move JS Files to src/js
 gulp.task('js', () => {
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
-                   'node_modules/jquery/dist/jquery.min.js',
-				   'node_modules/popper.js/dist/umd/popper.min.js'])
-						.pipe(gulp.dest('src/js'))
-						.pipe(browserSync.stream());
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
 });
 
-//Watch Sass & Server
+// Watch Sass & Server
 gulp.task('serve', ['sass'], () => {
-	browserSync.init({
-		server:	'./src'
-	});
-	gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss',
-				'src/scss/*.scss'],
-				['sass']);
-	gulp.watch('src/*.html').on('change', browserSync.reload);
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-//Move Fonts folder to src
+// Move Fonts Folder to src/fonts
 gulp.task('fonts', () => {
-	return gulp.src('node_modules/fonts-awesome/fonts/*')
-		pipe(gulp.dest('src/fonts'));
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest("src/fonts"));
 });
 
-//Move Font Awesome css to src/css
+// Move Font Awesome CSS to src/css
 gulp.task('fa', () => {
-	return gulp.src('node_modules/fonts-awesome/css/font-awesome.min.css')
-		pipe(gulp.dest('src/css'));
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest("src/css"));
 });
 
-//When we run gulp it runs all specified gulp tasks
-gulp.task('default', [
-	'js',
-	'serve',
-	'fa',
-	'fonts'
-]);
+gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
