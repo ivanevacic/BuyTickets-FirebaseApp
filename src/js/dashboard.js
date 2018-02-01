47640edeee0ff59f4307d777f3404d59cf8a6bf6
ivanevacic <ivanevacicweb@gmail.com>
2018-02-01 23:30:30 +0100

    Fixed 'destionation' typo in firebase and in 'createTicket' script

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,1 +1,10 @@
-alert('test');
+Tickets.on('value', function(firebaseResponse){
+  var ticket_table = $('#ticket_table');
+  ticket_table.find('tbody').empty();
+  firebaseResponse.forEach(function(ticketsSnapshot){
+    var ticketKey = ticketsSnapshot.key;
+    var ticket = ticketsSnapshot.val();
+    var tableRow = '<tr><td>' + ticket.starting_point + '</td><td>' + ticket.destination_point + '</td><td>' + ticket.date_created +'</td></tr>';
+    ticket_table.find('tbody').append(tableRow);
+  });
+});
