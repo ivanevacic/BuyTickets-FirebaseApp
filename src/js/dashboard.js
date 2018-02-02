6c0de9446363b61982638472a81598ad86bfa326
ivanevacic <ivanevacicweb@gmail.com>
2018-02-02 01:13:33 +0100

    Added 'DeleteTicket' function

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -4,7 +4,20 @@ Tickets.on('value', function(firebaseResponse){
   firebaseResponse.forEach(function(ticketsSnapshot){
     var ticketKey = ticketsSnapshot.key;
     var ticket = ticketsSnapshot.val();
-    var tableRow = '<tr><td>' + ticket.starting_point + '</td><td>' + ticket.destination_point + '</td><td>' + ticket.date_created +'</td></tr>';
+    var tableRow = '<tr><td>' + ticket.starting_point + '</td><td>' + ticket.destination_point + '</td><td>' + ticket.date_created + '</td><td><button type="button"  onclick="ViewTicketDetails(\'' + ticketKey + '\')" class="btn btn-xs btn-success">View details</button></td><td><button type="button" onclick="EditTicket(\'' + ticketKey + '\')" class="btn btn-xs btn-warning">Edit</button></td><td><button onclick="DeleteTicket(\'' + ticketKey + '\')" type="button" class="btn btn-xs btn-danger">Delete</button></td></tr>';
     ticket_table.find('tbody').append(tableRow);
   });
 });
+
+function ViewTicketDetails(ticketKey){
+
+}
+
+function EditTicket(ticketKey){
+
+}
+
+function DeleteTicket(ticketKey){
+  var ticketRef = firebaseBuyTickets.ref('Tickets/' + ticketKey);
+  ticketRef.remove();
+}
