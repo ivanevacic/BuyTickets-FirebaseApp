21910e50ec0f950529878b524ecb2b7b1ee1af2c
ivanevacic <ivanevacicweb@gmail.com>
2018-02-03 01:13:30 +0100

    Added currency to price

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -4,7 +4,7 @@ Tickets.on('value', function(firebaseResponse){
   firebaseResponse.forEach(function(ticketsSnapshot){
     var ticketKey = ticketsSnapshot.key;
     var ticket = ticketsSnapshot.val();
-    var tableRow = '<tr><td>' + ticket.starting_point + '</td><td>' + ticket.destination_point + '</td><td>' + ticket.date_created + '</td><td>' + ticket.distance + '</td><td>' + ticket.duration + '</td><td>' + ticket.price + '</td><td>' + ticket.ticket_type + '</td><td><button type="button"  onclick="ViewTicketDetails(\'' + ticketKey + '\')" class="btn btn-xs btn-success" data-toggle="modal" data-target="#ViewDetailsModal">View</button></td><td><button type="button" onclick="EditTicket(\'' + ticketKey + '\')" class="btn btn-xs btn-warning">Edit</button></td><td><button onclick="DeleteTicket(\'' + ticketKey + '\')" type="button" class="btn btn-xs btn-danger">Delete</button></td></tr>';
+    var tableRow = '<tr><td>' + ticket.starting_point + '</td><td>' + ticket.destination_point + '</td><td>' + ticket.date_created + '</td><td>' + ticket.distance + "km" + '</td><td>' + ticket.duration + '</td><td>' + ticket.price + "€" + '</td><td>' + ticket.ticket_type + '</td><td><button type="button"  onclick="ViewTicketDetails(\'' + ticketKey + '\')" class="btn btn-xs btn-success" data-toggle="modal" data-target="#ViewDetailsModal">View</button></td><td><button type="button" onclick="EditTicket(\'' + ticketKey + '\')" class="btn btn-xs btn-warning">Edit</button></td><td><button onclick="DeleteTicket(\'' + ticketKey + '\')" type="button" class="btn btn-xs btn-danger">Delete</button></td></tr>';
     ticket_table.find('tbody').append(tableRow);
   });
 });
